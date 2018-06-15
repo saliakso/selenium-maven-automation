23ec11fc273b404c87fbf736fbe1554c7bb9b4dd
saliakso <salihaksoy@yahoo.com>
2018-06-15 00:06:19 -0400

    print test time and date

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,6 @@
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -65,7 +66,7 @@ public class DiceJobSearch {
 			
 		}
 		driver.close();
-		
+		System.out.println("TEST Completed - "+ LocalDateTime.now());
 	}
 
 }
